f03a81cc927b3b8049c7ad6dfec28a280a96d169
Firman47 <122626748+Firman47@users.noreply.github.com>
2024-09-20 17:29:55 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,33 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        "light-blue": {
+          50: "#fafdfe",
+          100: "#f0f8fd",
+          200: "#e8f4fb",
+          300: "#def0fa",
+          400: "#d8edf9",
+          500: "#cee8f7",
+          600: "#bbd3e1",
+          700: "#92a5af",
+          800: "#718088",
+          900: "#576168",
+        },
+        "Dark-Blue": {
+          50: "#eaebf3",
+          100: "#bdc1da",
+          200: "#9da3c8",
+          300: "#7178af",
+          400: "#555ea0",
+          500: "#2b3688",
+          600: "#27317c",
+          700: "#1f2661",
+          800: "#181e4b",
+          900: "#181e4b",
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
